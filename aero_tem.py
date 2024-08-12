cc9fb43cd0a8d4109c439c3e5fe8a6355efaeed5
Lauren Meleney <LAMeleney@gmail.com>
2024-08-12 11:46:40 -0600

    first attempt at square milk bottle

--- aero_tem.py
+++ aero_tem.py
@@ -6,6 +6,10 @@ import matplotlib.animation as ani #importing animation library
 
 # from https://www.aeroodyssey.org/3d-heat-equation
 
+def calculate_dt(dx, alpha):
+	dt = 0.5 * dx**2 / (2*alpha) #dt needed for stability can do larger however good rule of thumb
+	return dt
+
 def frame(frame_number,Tout,plot):
     plot[0].remove()
     plot[0] = ax1.plot_surface(
@@ -69,40 +73,46 @@ def step_thru_time(n_time_steps, dt, Xvec, Yvec, alpha, T_start):
 
 if __name__ == "__main__":
 	
-	alpha = 97 # (mm^2/s) thermal diffusivity, alluminum
+	# set thermal diffusivity, in mm2 / s
+	alpha = 97 # aluminum
+	alpha = 0.143 # water
 	
-	#length of sides  of heated square plate
-	Lx = 152 #(mm)
-	Ly = 152 #(mm)
+	#length of sides of heated square plate (mm)
+	Lx = 100 #
+	Ly = 100 #
 	
 	#number of points
-	N = 50 #number of x and y points x*y = total number of points
+	N = 20 #number of x and y points x*y = total number of points
 	
 	#Discretize space
-	Xvec = np.linspace(0,Lx,N)
-	Yvec = np.linspace(0,Ly,N)
-	dx = Xvec[2]-Xvec[1] #since equally spaced, i set a constant dx
-	dy = Yvec[2]-Yvec[1] #since equally spaced, I set a constant dy
+	Xvec = np.linspace(0, Lx, N)
+	Yvec = np.linspace(0, Ly, N)
+	dx = Xvec[2] - Xvec[1] #since equally spaced, i set a constant dx
+	dy = Yvec[2] - Yvec[1] #since equally spaced, I set a constant dy
 	
 	#Discretize time
-	#dt=0.5*(dx**2)/(2*alpha) #dt needed for stability can do larger however good rule of thumb
-	dt = 0.025
-	n_time_steps = 200
-	t_end = n_time_steps * dt
+	dt = calculate_dt(dx, alpha)
+	print(dt)
+	t_end = 600
+	n_time_steps = int(t_end / dt)
 
 	# inital conditions
-	T_init = 20
+	T_init = 2 # fridge
 	T = np.full((Yvec.size,Xvec.size), T_init) #entire plate is at 20 degrees C
 
 	# boundary conditions
-	T[:,0] = 200.0 #200 degrees C applied to left side of plate
-	T[-1,:] = 200.0 #200 degrees C applied to bottom of plate
-	T[0,:] = 200.0 #200 degrees C applied to top of plate
-	T[:,-1] = 200.0 #200 degrees C applied to right side of plate
+	# note: need to handle non-temperature boundary conditions
+	T_boundary = 80 #C
+	T[:,0] = T_boundary # left side of plate
+	T[-1,:] = T_boundary # bottom
+	T[0,:] = T_boundary # top
+	T[:,-1] = T_boundary # right
 
 	Tout = step_thru_time(n_time_steps, dt, Xvec, Yvec, alpha, T)
 
 	print('made Tout')
+
+	# add time text in animation
 
 	fig = plt.figure()
 	ax1 = plt.axes(projection="3d")
@@ -111,7 +121,7 @@ if __name__ == "__main__":
 	ax1.set_zlabel('Temperature (C)')
 	ax1.set_xlim(0, Lx)
 	ax1.set_ylim(0, Ly)
-	ax1.set_zlim(0, 200)
+	ax1.set_zlim(0, 100)
 	X, Y = np.meshgrid(Xvec, Yvec)
 	plot1=[ax1.plot_surface(X, Y, Tout[0,:,:])]
 
@@ -120,7 +130,7 @@ if __name__ == "__main__":
 		func = frame,
 		frames = n_time_steps,
 		fargs = (Tout, plot1),
-		interval = 60,
+		interval = 400,
 		blit = False
 		)
 	# plt.show()
@@ -130,7 +140,7 @@ if __name__ == "__main__":
 	print('made animation')
 
 	# save to file
-	animation.save(filename = 'test.gif', writer = 'pillow')
+	animation.save(filename = 'square_milk_bottle_draft.gif', writer = 'pillow')
 
 	print('saved animation')
 
