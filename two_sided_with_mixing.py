e439b1ed764793b454d53ff5a31297207211a15d
thomasemurphy <tommurphyemail@gmail.com>
2024-08-12 17:10:57 -0600

    script for conduction thru glass bottle

--- two_sided_with_mixing.py
+++ two_sided_with_mixing.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from numpy import pi
 
 # hot side temperature
@@ -35,3 +36,32 @@ area_side_bottle = 2 * pi * radius_bottle * milk_height
 
 area_total_ht = area_bottom_bottle + area_side_bottle
 
+bottle_thickness = .003 #m  3mm?
+
+conduction_term_lumped = conductivity_glass * area_total_ht / bottle_thickness
+
+capacitance_term_lumped = density_cold_side * cv_cold_side * volume_cold_side_m3
+
+max_time = 300 # seconds
+
+dt = 1 # second
+
+n_time_steps = int(max_time / dt)
+
+time_temp_df = pd.DataFrame(columns = ['t', 'T_h', 'T_c', 'q', 'dThdt', 'dTcdt', 'dTh', 'dTc'])
+
+time_temp_df.loc[0, 't'] = 0
+time_temp_df.loc[0, 'T_h'] = T_h
+time_temp_df.loc[0, 'T_c'] = T_c_i
+
+for it in range(n_time_steps):
+	time_temp_df.loc[it, 'q'] = conduction_term_lumped * (time_temp_df['T_h'][it] - time_temp_df['T_c'][it])
+	time_temp_df.loc[it, 'dThdt'] = 0
+	time_temp_df.loc[it, 'dTcdt'] = time_temp_df['q'][it] / capacitance_term_lumped
+	time_temp_df.loc[it, 'dTh'] = 0
+	time_temp_df.loc[it, 'dTc'] = time_temp_df['dTcdt'][it] * dt
+	time_temp_df.loc[it + 1, 't'] = time_temp_df['t'][it] + dt
+	time_temp_df.loc[it + 1, 'T_h'] = T_h
+	time_temp_df.loc[it + 1, 'T_c'] = time_temp_df['T_c'][it] + time_temp_df['dTc'][it]
+
+time_temp_df.to_csv('milk_temp.csv')
